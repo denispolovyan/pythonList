43175320d528acbd6e987d03bb550697246317c1
denispolovyan <denispolovyan@gmail.com>
2023-07-19 20:43:02 +0300

    feature: added validation for value user wanna find and change

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 arr = [21, 2, 7, 4, 9]
-choice = input('\nYou can change el by index (i) or by value (v)\nChoose your route: ')
 counter = 0
+coincidence = False
 
+choice = input('\nYou can change el of array by index (i) or by value (v)\nChoose your route: ')
 while choice != 'i' and choice != 'v':
-    choice = input('\tError. Enter i or v: ')
+    choice = input('\t\tError. Enter i or v: ')
 
 print('Array: ', arr)
 
@@ -17,6 +18,12 @@ if choice == 'i':
     arr[oldNum - 1] = newNum
 else:
     oldNum = int(input('\tEnter value of array you wanna change: '))
+    while not coincidence:
+        for el in arr:
+            if oldNum == el:
+                coincidence = True
+        if not coincidence:
+            oldNum = int(input('\t\tError. Enter existing value: '))
     newNum = int(input('\tEnter value of array you wanna set: '))
 
     for el in arr:
@@ -27,4 +34,4 @@ else:
     arr.insert(counter, newNum)
     arr.remove(oldNum)
 
-print(arr)
+print('Array: ', arr)
